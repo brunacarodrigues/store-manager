93e918a4d4e2a28a132342574e43907ce52b4851
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-07-19 14:08:35 -0300

    task 04 ok

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const { validateProductName } = require('../middlewares/validateProductName');
 
 const productsRoute = express();
 
 productsRoute.get('/', productsController.getAllProducts);
 productsRoute.get('/:id', productsController.getByIdProducts);
-productsRoute.post('/', productsController.createProductByName);
+productsRoute.post('/', validateProductName, productsController.createProductByName);
 
 module.exports = productsRoute;
